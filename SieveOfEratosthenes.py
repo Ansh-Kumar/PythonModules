4c9f48ed80570a78fb00706c3e908cf876473779
Ansh Kumar Khalasi <39108634+Ansh-Kumar@users.noreply.github.com>
2020-07-03 19:25:10 -0700

    Removed time from this function.

--- SieveOfEratosthenes.py
+++ SieveOfEratosthenes.py
@@ -3,7 +3,6 @@ import time
 
 
 def sieveOfEratosthenes(n):
-    startTime = time.time()
     notPrimeList = []
     primeList =[]
     for x in range(2, n+1):
@@ -11,8 +10,6 @@ def sieveOfEratosthenes(n):
             primeList.append(x)
             for i in range(x*x, n+1, x):
                 notPrimeList.append(i)
-    elapsedTime = round(time.time() - startTime, 2)
     print(primeList)
-    print(elapsedTime)
     
 sieveOfEratosthenes()
